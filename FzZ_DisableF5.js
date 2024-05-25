eeb546985f89ce0d8af858c0646036f1ce0dad99
Fzz00 <68388527+Fzz90@users.noreply.github.com>
2024-05-25 13:24:31 +0700

    Update FzZ_DisableF5.js

--- FzZ_DisableF5.js
+++ FzZ_DisableF5.js
@@ -13,13 +13,4 @@
   SceneManager.onKeyDown = function (event) {
     if (event.keyCode !== 116) alias.call(this, event);
   };
-
-  // _Graphics_defaultStretchMode_Alias = Graphics._defaultStretchMode;
-  // Graphics._defaultStretchMode = function () {
-  //   if (disable_F3 == true) {
-  //     return true;
-  //   } else {
-  //     _Graphics_defaultStretchMode_Alias.call(this);
-  //   }
-  // };
 })();
